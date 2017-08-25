d0b650b68034fb76024fb01dd53ccfd68556c733
David Monnerat <dmonnerat@users.noreply.github.com>
2017-08-25 18:44:06 -0400

    Added Rekognition

--- motion_detector.py
+++ motion_detector.py
@@ -76,16 +76,106 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		text = "Occupied"
 
-        # convert image into numpy array
-        data = np.fromstring(frame.getvalue(), dtype=np.uint8)
-        # turn the array into a cv2 image
-        orig = cv2.imdecode(data, 1)
-        print "Preparing image for Rekognition"
-
 	# draw the text and timestamp on the frame
 	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
 	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,0.35, (0, 0, 255), 1)
+
+	# check to see if the room is occupied
+	if text == "Occupied":
+		# check to see if enough time has passed between uploads
+		if (timestamp - lastUploaded).seconds >= 10:
+			# increment the motion counter
+			motionCounter += 1
+
+			# check to see if the number of frames with consistent motion is
+			# high enough
+			if motionCounter >= 5:
+				# if there is enough motion, send the frame to Rekognition
+
+                print "Preparing image for Rekognition"
+                # convert image into numpy array
+                data = np.fromstring(frame, dtype=np.uint8)
+                # turn the array into a cv2 image
+                orig = cv2.imdecode(data, 1)
+
+				# update the last uploaded timestamp and reset the motion
+				# counter
+				lastUploaded = timestamp
+				motionCounter = 0
+
+                # Detect the items in the image
+                print "Resizing image"
+                r = 1000.0 / orig.shape[1]
+                dim = (100, int(orig.shape[0] * r))
+                # perform the actual resizing of the image and show it
+                #img = cv2.resize(orig, (0,0), fx = 0.5, fy = 0.5)
+                img = orig
+
+                #imgWidth = 3857
+                #imgHeight = 2571
+                imgHeight, imgWidth, imgChannels = img.shape
+                print "Resized to " + str(imgHeight) + "x" + str(imgWidth)
+
+                print "Sending to Rekognition"
+
+                knownFaces = 0
+                unknownFaces = 0
+
+                results = rek.detect_faces(
+                    Image={
+                        'Bytes': cv2.imencode('.jpg', img)[1].tostring()
+                    },
+                    Attributes=['ALL']
+                )
+
+                iter = 0
+
+                print "Got back " + str(len(results["FaceDetails"])) + " faces"
+
+                # Print a message for each item
+                for face in results["FaceDetails"]:
+                    msg = "I found a {gender} who is {emot}".format(gender=face['Gender']['Value'], emot=face['Emotions'][0]['Type'].lower())
+
+                    if face['Smile']['Value'] is False:
+                        msg += " but they are not smiling"
+                    else:
+                        msg += " and they are smiling"
+                    print msg
+
+                    top = int(math.floor(face['BoundingBox']['Top'] * imgHeight))
+                    left = int(math.floor(face['BoundingBox']['Left'] * imgWidth))
+                    bottom = int(math.ceil(top + (face['BoundingBox']['Height'] * imgHeight)))
+                    right = int(math.ceil(left + (face['BoundingBox']['Width'] * imgWidth)))
+
+                    print "Crop boundaries: " + str(top) + ":" + str(bottom) + " " + str(left) + ":" + str(right)
+                    crop_img = img[top:bottom, left:right]
+                    cv2.imwrite('dave/face' + str(iter) + '.jpg',crop_img)
+                    iter=iter+1
+
+                    print "Sending face to rekognizer"
+
+                    try:
+                        faceResults = rek.search_faces_by_image(
+                            Image={
+                                'Bytes': cv2.imencode('.jpg', crop_img)[1].tostring()
+                            },
+                            CollectionId="family_collection"
+                        )
+
+                        if (len(faceResults['FaceMatches'])>0):
+                            knownFaces=knownFaces+1
+                            print "Hello, " + (faceResults['FaceMatches'][0]['Face']['ExternalImageId'])
+                        else:
+                            unknownFaces=unknownFaces+1
+
+                    except:
+                        unknownFaces=unknownFaces+1
+                        #print("Unexpected error:", sys.exc_info()[0])
+
+	# otherwise, the room is not occupied
+	else:
+		motionCounter = 0
 
 	# display the security feed
 	cv2.imshow("Security Feed", frame)
